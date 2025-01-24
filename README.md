36e58605b2c83d20c5d70de57ac08721661bc41a
Sam Nicholson <161619204+SamNich-Hippo@users.noreply.github.com>
2025-01-24 14:27:01 +0000

    [SWIP-139] Add header/footer (#1)
    * init commit
    
    works includes header/footer
    
    * Delete style.cssZone.Identifier
    
    * comments
    
    * readme/licence/codeowners
    
    * comments
    
    * Update version.php

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# govuk-moodle-theme
-A GOV.UK Moodle theme plugin
+# GOV.UK Moodle Theme
+A GOV.UK Moodle theme implementing [GOV GDS](https://design-system.service.gov.uk/)
+
+# Setup
+To use this theme you need to copy the the `gov-uk` directory into your moodle sites theme folder `\my-moodle-site\public\theme`. Moodle will then detect the added theme when started. You will need to confirm the installation. Once installed you will be able to select the `GOV.UK` theme from the theme selection page.
---
+++ govuk/lang/en/theme_govuk.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme functions.
+ *
+ * @package    theme_govuk
+ * @copyright  Copyright (c) Crown Copyright (Department for Education)
+ * @license    https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/ Open Government Licence v3.0
+ */
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();                                                                                                
+                                                                                                                                    
+// A description shown in the admin theme selector.                                                                                 
+$string['choosereadme'] = 'Theme GOV.UK is a child theme of Boost';                
+// The name of our plugin.                                                                                                          
+$string['pluginname'] = 'GOV.UK';                                                                                                    
+// We need to include a lang string for each block region.                                                                          
+$string['region-side-pre'] = 'Right';
