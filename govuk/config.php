5e72217d480c09fd185f5981b78ed1f5e6b5b9a2
Brad Park (Igi) <brad@igi.codes>
2025-07-24 13:40:16 +0100

    SWIP-868 - GovUK Rebrand (#23)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- govuk/config.php
+++ govuk/config.php
@@ -29,3 +29,11 @@ $THEME->name = 'govuk';
 $THEME->parents = ['boost'];
 $THEME->sheets = ['govuk'];
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
+
+$THEME->layouts = [
+    'standard' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+];
