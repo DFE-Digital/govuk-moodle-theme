ef63097eb7d3c9a40f61581f5e07cb4f17bf6482
Sam Nicholson <sam.nicholson@hippodigital.co.uk>
2025-09-08 11:43:21 +0100

    Add logout functionality and service name

--- govuk/renderers.php
+++ govuk/renderers.php
@@ -1,7 +1,18 @@
 <?php
 
 class theme_govuk_core_renderer extends core_renderer
-{
+{ 
+    public function logout_endpoint($withlinks = true) {
+        global $USER;
+
+        if (!isloggedin() || isguestuser()) {
+            return '';
+        }
+
+        $logouturl = new \moodle_url('/login/logout.php', ['sesskey' => sesskey()]);
+        return $logouturl->out(false);
+    }
+
     public function body_attributes($additionalclasses = [])
     {
         $attributes = parent::body_attributes($additionalclasses);
