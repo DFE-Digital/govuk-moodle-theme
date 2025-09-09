e8d3f046228e4ffd385b2e304d442a1321a074fe
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-09 11:39:16 +0100

    SWIP-1044 Remove breadcrumb on course pages (#29)
    * Remove breadcrumb on course pages
    
    * Apply Prettier formatting fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- govuk/renderers.php
+++ govuk/renderers.php
@@ -67,4 +67,16 @@ class theme_govuk_core_renderer extends core_renderer
         }
         return $firstview;
     }
+
+    /** Hide breadcrumbs on pages within a course */
+    public function navbar(): string
+    {
+        if (
+            $this->page->context &&
+            $this->page->context->get_course_context(false)
+        ) {
+            return '';
+        }
+        return parent::navbar();
+    }
 }
