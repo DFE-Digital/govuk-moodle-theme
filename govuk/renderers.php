8fdb9b6d131b477a4f69a47d2122e1d07fb7cf7c
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-10 17:09:06 +0100

    SWIP-1054 Disable sticky footer, change buttons on add entry (#33)
    * Disable sticky footer, change buttons on add entry
    
    Also a tweak to the header text
    
    * Cancel url logic
    
    * Use back to param
    
    * Apply Prettier formatting fixes
    
    * Add phase banner for SWIP-1042
    
    * Add code removed in error
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- govuk/renderers.php
+++ govuk/renderers.php
@@ -79,4 +79,49 @@ class theme_govuk_core_renderer extends core_renderer
         }
         return parent::navbar();
     }
+
+    /**
+     * Override to change content used to render the sticky footer template
+     * on the add entry page for database activities (mod/data/edit.php)
+     *
+     * @param \core\output\sticky_footer $footer
+     * @return string
+     */
+    protected function render_sticky_footer(
+        \core\output\sticky_footer $footer,
+    ): string {
+        $data = $footer->export_for_template($this);
+
+        $isdataedit = $this->page->pagetype === 'mod-data-edit';
+        $rid = optional_param('rid', 0, PARAM_INT);
+        $isadd = $rid == 0;
+
+        if ($isdataedit && $isadd) {
+            // include cancel and save buttons
+            $backto = optional_param('backto', '', PARAM_LOCALURL);
+            if (!empty($backto)) {
+                $cancelurl = new \moodle_url($backto);
+            } else {
+                $cmid = $this->page->cm
+                    ? $this->page->cm->id
+                    : optional_param('id', 0, PARAM_INT);
+                $cancelurl = new \moodle_url('/mod/data/view.php', [
+                    'id' => $cmid,
+                ]);
+            }
+            $data['stickycontent'] = html_writer::link(
+                $cancelurl,
+                get_string('cancel'),
+                ['class' => 'btn btn-secondary mx-1', 'role' => 'button'],
+            );
+            $data['stickycontent'] .= html_writer::empty_tag('input', [
+                'type' => 'submit',
+                'name' => 'saveandview',
+                'value' => get_string('save'),
+                'class' => 'btn btn-primary mx-1',
+            ]);
+        }
+
+        return $this->render_from_template('core/sticky_footer', $data);
+    }
 }
